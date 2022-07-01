4cd57282cea31b0fb5fee4f4d76717c4944f7e24
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-07-01 16:37:40 -0700

    firs commit set up initial files and question array

---
+++ script.js
@@ -0,0 +1,39 @@
+//make an array with all the quiz questions 
+const quizData = [
+    {
+        question: 'Inside which HTML element does the JS link belong?',
+        a: 'the good part',
+        b: 'the header',
+        c: 'the bottom of the body',
+        d: 'both B and C',
+        correct: 'd'
+    }, {
+        question: 'How do you grab an HTML element by its ID in Javascript?',
+        a: 'document.getElementById',
+        b: 'document.getThatId',
+        c: 'get.elementid',
+        d: 'document.elementId',
+        correct: 'a'
+    }, {
+        question: 'How would you write a message in an alert box?',
+        a: 'alert(message goes here)',
+        b: 'alert message[message goes here]',
+        c: 'alertbox(message)',
+        d: 'alertmsg{message}',
+        correct: 'a'
+    }, {
+        question: 'What is the purpose of a for loop?',
+        a: 'Getting fruit loops from the store',
+        b: 'repeating a block of code for a known number of times',
+        c: 'adding the css properties to the file',
+        d: 'storing data',
+        answer: 'b'
+    }, {
+        question: 'How do you create a function in Javascript?',
+        a: 'function = myFunction[]',
+        b: 'function: function',
+        c: 'function = myFunction()',
+        d: 'function.start = functionName',
+        correct: 'c'
+    }
+];
